d6226fb1642c9b8a4028179339c396c0ecb95196
Sebastian Skonieczny <85199759@bat.com>
2025-02-19 16:49:09 +0100

    Light and dark mode - Added a button switch in the top left corner to toggle light and dark mode - Added aitalents logo next to data analyzer text - Added css variables for colors

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
+import logoaitalents from "../assets/logoaitalents.png"
 function Navbar() {
-    return (
-      <nav className="navbar">
-        <div className="navbar-content">
-          <div className="navbar-right">
-            <span className="name">Data Analyzer</span>
-          </div>
-        </div>
-      </nav>
-    );
+  const [lightMode, setLightMode] = useState(true);
+  function toggleMode(){
+    if(lightMode){
+      document.body.classList.add('darkmode')
+      setLightMode(false);
+    }
+    else{
+      document.body.classList.remove('darkmode');
+      setLightMode(true);
+    }
   }
-  
-  export default Navbar;
-  
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <div className="light-dark-mode-switch" onClick={toggleMode}>
+          <span className="material-symbols-outlined">
+            { lightMode ? "light_mode" : "dark_mode"}
+          </span>
+
+        </div>
+      </div>
+
+      <div className="navbar-right">
+
+        <span className="name">Data Analyzer</span>
+      </div>
+      <img src={logoaitalents} alt="" className="aitallogo"></img>
+
+    </nav>
+  );
+}
+
+export default Navbar;
